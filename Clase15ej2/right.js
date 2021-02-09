2691c74dd284038769577fc34fecd7eeb4d1ebc0
antonio <antoniobique05@gmail.com>
2021-02-09 14:32:06 -0500

    Tarea onclick

--- Clase15ej2/right.js
+++ Clase15ej2/right.js
@@ -2,18 +2,14 @@ let boton3= document.querySelector("#Right")
 let parrafo3= document.querySelector("#parrafo3")
 boton3.onclick= cambiacolor
 function cambiacolor(){
-  if(parrafo3.style.color == "black")
+  if(parrafo3.innerHTML =="Hemos visto que en tiempo de Cervantes la Astronomía estaba en plena revolución, mientras la Química, la Medicina y la Biología tendrían que esperar aún varios siglos hasta que les llegase el turno." )
    {
-      parrafo3.style.color= "blue"
-      parrafo3.innerHTML="Hemos visto que en tiempo de Cervantes la Astronomía estaba en plena revolución, mientras la Química, la Medicina y la Biología tendrían que esperar aún varios siglos hasta que les llegase el turno."
-      parrafo3.style.display= "none"
+      parrafo3.innerHTML= ""
       setTimeout(cambiacolor,3000)
     }
   else 
   {
-      parrafo3.style.color= "black"
-      parrafo3.style.display="block"
+      parrafo3.innerHTML="Hemos visto que en tiempo de Cervantes la Astronomía estaba en plena revolución, mientras la Química, la Medicina y la Biología tendrían que esperar aún varios siglos hasta que les llegase el turno."
   }
 }
-boton3.style.color= "black"
-console.log(parrafo3.style.display)
+parrafo3.innerHTML="Hemos visto que en tiempo de Cervantes la Astronomía estaba en plena revolución, mientras la Química, la Medicina y la Biología tendrían que esperar aún varios siglos hasta que les llegase el turno."
